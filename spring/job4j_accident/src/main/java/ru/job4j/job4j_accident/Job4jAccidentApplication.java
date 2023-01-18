9c4db1fc3a018488eb1930b2ae52cdaa81645145
bespalov0928 <bespalov0928@mail.ru>
2023-01-18 10:27:08 +0700

    job4j_accident: Создание нового проекта.

---
+++ spring/job4j_accident/src/main/java/ru/job4j/job4j_accident/Job4jAccidentApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.job4j_accident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jAccidentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jAccidentApplication.class, args);
+    }
+
+}
